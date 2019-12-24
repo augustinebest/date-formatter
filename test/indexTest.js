a7d8b800421a468be0db31a00a5bbf674bba2578
augustinebest <augustusbest01@gmail.com>
2019-12-24 23:25:45 +0100

    my testing is working fine using mocha and chai

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,11 +1,26 @@
-const assert = require('chai').assert;
+const expect = require('chai').expect;
 const { formatDate, formatTime } = require('../index');
 
+let timeStamp = new Date();
+let format_time = '12h';
+let format_date = 'others'
+
 describe('Index', function() {
+    // Testing for Time
     describe('formatTime()', function() {
         it('format to return 00:00 xx || 00:00', function() {
-            let result = formatTime();
-            assert.equal(result, 'hello')
+            let result = formatTime(timeStamp, format_time);
+            let regex = /\d+:\d+ ?(am|pm)?/;
+            expect(result).to.match(regex);
+        })
+    })
+    
+     // Testing for date
+     describe('formatDate()', function() {
+        it('format to return dd/mm/yyyy || mm/dd/yyyy || yyyy/mm/dd || Tuesday 24th of December, 2019', function() {
+            let result = formatDate(timeStamp, format_date);
+            let regex = /(\d+\/\d+\/\d+)|(\w+ \d+\w+ \w+ \w+, \d+)/;
+            expect(result).to.match(regex);
         })
     })
 })
